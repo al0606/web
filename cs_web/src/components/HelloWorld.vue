5d916dbb338d420f091c32adb34591c59ebee065
lv <lvyuting@shunyiedu.com>
2025-12-09 09:17:47 +0800

    [+]创建vue项目

---
+++ cs_web/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout>
+    <a-layout-header>Header</a-layout-header>
+    <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout-content>Content</a-layout-content>
+    </a-layout>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
+</template>
+
+<script>
+export default {
+  name: 'AppLayout'
+}
+</script>
