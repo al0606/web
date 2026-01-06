fddb1091a7c889ffd7a9a8794ebcce3dacc6f47d
lv <lvyuting@shunyiedu.com>
2026-01-06 22:44:20 +0800

    [^]完善

--- src/components/GlobalHeader/RightContent.vue
+++ src/components/GlobalHeader/RightContent.vue
@@ -1,5 +1,30 @@
 <template>
   <div :class="wrpCls">
+    磨损转换：
+    <a-select
+        placeholder="请输入名称查询商品"
+        style="width: 200px"
+        show-search
+        :default-active-first-option="false"
+        :show-arrow="false"
+        :filter-option="false"
+        :not-found-content="null"
+        @search="handleSearch"
+        allowClear
+        @select="selected"
+    >
+      <a-select-option v-for="item in goodsList" :key="item.id" :value="item.id">{{ item.name }}</a-select-option>
+    </a-select>
+    <a-input-number
+        placeholder="请输入磨损值"
+        style="width: 150px;"
+        @change="changeWear(goods, $event)"
+        :value="wear"
+        :min="goods.minWear"
+        :max="goods.maxWear"
+        :step="0.0001"
+    />
+    结果：{{ realWear }}
     <avatar-dropdown :menu="showMenu" :current-user="currentUser" :class="prefixCls" />
     <select-lang :class="prefixCls" />
   </div>
@@ -8,6 +33,7 @@
 <script>
 import AvatarDropdown from './AvatarDropdown'
 import SelectLang from '@/components/SelectLang'
+import { findAllGoods } from '@/api/warehouse'
 
 export default {
   name: 'RightContent',
@@ -36,7 +62,11 @@ export default {
   data () {
     return {
       showMenu: true,
-      currentUser: {}
+      currentUser: {},
+      wear: 0,
+      realWear: 0,
+      goods: {},
+      goodsList: []
     }
   },
   computed: {
@@ -53,6 +83,22 @@ export default {
         name: 'Serati Ma'
       }
     }, 1500)
+  },
+  methods: {
+    handleSearch (keyword) {
+      if (!keyword) return
+      findAllGoods(keyword).then(res => {
+        this.goodsList = res
+      })
+    },
+    changeWear (goods, value) {
+      this.wear = value
+      // 真实磨损值（用于计算）= 1 / (最大磨损值 - 最小磨损值) * (当前材料磨损值 - 最小磨损值)
+      this.realWear = (1 / (goods.maximumWear - goods.minimumWear) * (value - goods.minimumWear)) || 0
+    },
+    selected (value) {
+      this.goods = this.goodsList.find(item => item.id === value)
+    }
   }
 }
 </script>
